8128de77b66ce7892525d7a1c4c7597d1c4c8756
Sanjeev <gsanjeevtripathi@gmail.com>
2018-01-13 17:47:13 +0530

    JPA MySQL with Rest Controller

---
+++ JPA_Hibernate_Mysql/actorsRestSvc/src/main/java/com/example/laxtech/actors/ActorsApplication.java
@@ -0,0 +1,14 @@
+package com.example.laxtech.actors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ActorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsApplication.class, args);
+	}
+}
