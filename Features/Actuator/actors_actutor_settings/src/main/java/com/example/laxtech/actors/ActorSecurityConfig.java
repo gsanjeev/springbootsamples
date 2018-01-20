908e895f5ed07a817dec271f1ccc9c97ac88c016
Sanjeev <gsanjeevtripathi@gmail.com>
2018-01-20 10:07:27 +0530

    actuator

---
+++ Features/Actuator/actors_actutor_settings/src/main/java/com/example/laxtech/actors/ActorSecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.laxtech.actors;
+
+/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity*/
+public class ActorSecurityConfig {//extends WebSecurityConfigurerAdapter {
+/*    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("actuator").password("actuator").roles("ACTUATOR");
+    }*/
+}
