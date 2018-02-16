5b3d248f1e2810d2b92af2ad3bee0b22729cc9dd
Sanjeev <gsanjeevtripathi@gmail.com>
2018-02-16 18:15:00 +0530

    elasticsearch

---
+++ elasticsearch/LIS/src/main/java/com/example/laxtech/LIS/ElasticsearchConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.laxtech.LIS;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.mkyong.book.repository")
+public class ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Value("${elasticsearch.port}")
+    private int elasticsearchPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String elasticsearchClusterName;
+
+    @Bean
+    public Client client() throws Exception {
+
+        Settings elasticsearchSettings = Settings.builder().put("cluster.name", elasticsearchClusterName)
+                .build();
+
+        Settings settings = Settings.builder()
+                .put("cluster.name", elasticsearchClusterName).build();
+        TransportClient transportClient = new PreBuiltTransportClient(settings);
+        transportClient.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(elasticsearchHost), elasticsearchPort));
+        //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
+        return transportClient;
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
+        return new ElasticsearchTemplate(client());
+    }
+
+    //Embedded Elasticsearch Server
+    /*@Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
+    }*/
+
+}
